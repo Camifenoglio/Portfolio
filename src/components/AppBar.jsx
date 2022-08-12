49c8bfdd701be450d68ca72bd54dc85afdbba756
Camifenoglio <cami.fenoglio@gmail.com>
2022-08-12 20:03:35 -0300

    portfolio

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,13 @@
+
+function AppBar() {
+    return (
+        <div className='appBar'>
+            <h1 className="textoAppBar">Camila Lucero Fenoglio</h1>
+            <h1 className="textoAppBar barra">|</h1>
+            <h1 className="textoAppBar">Desarrolladora FullStack MERN & Mobile Apps</h1>
+        </div>
+
+    );
+}
+
+export default AppBar;
